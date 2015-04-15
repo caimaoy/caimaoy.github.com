672066333810220bef029ffe5de96f30c19e4b76
caimaoy <caimaoy@gmail.com>
2015-04-15 20:51:54 +0800

    update

--- add_cname.bat
+++ add_cname.bat
@@ -1,3 +1,3 @@
 echo caimaoy.com > %1/CNAME
 cp google0dab5e3ca1b7fa15.html  %1/google0dab5e3ca1b7fa15.html
-cp baidu_verify_fZeT7Gyz3G.html %!/baidu_verify_fZeT7Gyz3G.html
+cp baidu_verify_fZeT7Gyz3G.html %1/baidu_verify_fZeT7Gyz3G.html
